631a3b6416bde13d5b5ec3580d1158aa05bc0b15
Nora <nschlueter@gmail.com>
2023-11-02 11:59:27 -0600

    if you need other functions I'll make them later

--- mathshiet.py
+++ mathshiet.py
@@ -1,4 +1,17 @@
-#welcome to math shiet
+#welcome to math shiet, where Noah imports the easy functions
+#Or he makes me make complicated ones if he's mean
+
+def add(num1 = 0, num2 = 0) :
+    return (num1 + num2)
+
+def subtract(num1 = 0, num2 = 0) :
+    return (num1 - num2)
+
+def multiply(num1 = 0, num2 = 0) :
+    return (num1 * num2)
+
+def divide(num1 = 0, num2 = 1) :
+    return(num1 / num2)
 
 
 #oh waw thank you soooooooooooo much -- world's most sarcastic man
